0f09d12dc043c4ebaed17640652b39115edb65c5
Zh-maxim <77917179+Zh-maxim@users.noreply.github.com>
2023-09-03 21:51:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let background = document.querySelector(".obolochka")
+console.log(background)
+let first = () => {
+    background.style.background = "brown"
+}
+let second = () => {
+    background.style.background = "orange"
+}
+setInterval(first, 800)
+setInterval(second, 1600)
+//'Если тебя это не устраивает, то кидай меня в БАН И ДОКАЖИ ЧТО ТЕПЕРЬ ПОХУЙ НА МНЕНИЕ ДРУГИХ ЛЮДЕЙ, ЧТО ТЫ ИГОИСТИЧНОЕ ЧМО !!!' - один из лидеров восстания
+let spam = () => {
+    alert('Если тебя это не устраивает, то кидай меня в БАН И ДОКАЖИ ЧТО ТЕПЕРЬ ПОХУЙ НА МНЕНИЕ ДРУГИХ ЛЮДЕЙ, ЧТО ТЫ ИГОИСТИЧНОЕ ЧМО !!! - один из лидеров восстания')
+    alert('Один банан сейчас это читает и ему либо похуй, либо он вахуе, другой эмоции не дано')
+}
+spam()
